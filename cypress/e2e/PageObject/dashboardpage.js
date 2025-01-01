056a733d75c40cecc9c62438e18542bdc0561075
Dhananjay <dhananjayshivannaj@gmail.com>
2025-01-01 19:41:25 +0530

    updated applyleave

--- cypress/e2e/PageObject/dashboardpage.js
+++ cypress/e2e/PageObject/dashboardpage.js
@@ -6,7 +6,7 @@ class dashboardPage{
         return 'a[href="/web/index.php/performance/viewPerformanceModule"]'
     }
     timeandWorkHeader(){
-        return ' abcd'
+        return 'abcd'
     }
 
 }
